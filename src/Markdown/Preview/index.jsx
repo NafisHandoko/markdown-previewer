a103987e945c9332604b3ed8a2b759e21a0ab3a7
Nafis Arinda Rizky Putra Handoko <69886565+NafisHandoko@users.noreply.github.com>
2021-10-16 06:01:44 +0000

    add breaks: true to marked option to convert CR to <br>

--- src/Markdown/Preview/index.jsx
+++ src/Markdown/Preview/index.jsx
@@ -5,7 +5,7 @@ import marked from 'marked';
 class Preview extends Component{
   render(){
     return(
-      <div id="preview" dangerouslySetInnerHTML={{ __html: marked(this.props.output,{sanitize: true}) }}>
+      <div id="preview" dangerouslySetInnerHTML={{ __html: marked(this.props.output,{sanitize: true,breaks: true}) }}>
       </div>
     )
   }
